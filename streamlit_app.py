3b24277e92c0e824792f2d951736ad410c0a8a76
Piyush Kanojia <80689391+piyushkanojia@users.noreply.github.com>
2023-04-25 14:20:56 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit
 streamlit.title('My parents new healthy diner')
-streamlit.header('Breakfast menu')
-streamlit.text('Vada Pav')
-streamlit.text('Samosa Pav')
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
