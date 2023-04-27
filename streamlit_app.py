cec68b3d75dfac953740dcb71f23c15d05fed18b
Piyush Kanojia <80689391+piyushkanojia@users.noreply.github.com>
2023-04-27 11:33:36 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
-from urllib.error import urlerror
+#from urllib.error import urlerror
 streamlit.title('My parents new healthy diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
